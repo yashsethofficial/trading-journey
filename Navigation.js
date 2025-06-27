93fd7ebf7cd4e2f66e5ac0662431de76c5da6fb0
yashsethofficial <yashsethofficial@gmail.com>
2025-06-27 22:39:23 +0530

    updated code"

--- Navigation.js
+++ Navigation.js
@@ -2,7 +2,6 @@ import React from 'react';
 import { NavigationContainer, DefaultTheme } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import DashboardScreen from './screens/DashboardScreen';
-import AddTradeScreen from './screens/AddTradeScreen';
 
 const Stack = createNativeStackNavigator();
 
@@ -23,7 +22,6 @@ export default function Navigation({ theme }) {
     <NavigationContainer theme={navTheme}>
       <Stack.Navigator initialRouteName="Dashboard">
         <Stack.Screen name="Dashboard" component={DashboardScreen} />
-        <Stack.Screen name="Add Trade" component={AddTradeScreen} />
       </Stack.Navigator>
     </NavigationContainer>
   );
